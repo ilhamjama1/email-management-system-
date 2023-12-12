088cd4f77f4c2c87efb1ec32ba226d836dc36c92
ilhamjama1 <153144348+ilhamjama1@users.noreply.github.com>
2023-12-12 16:02:42 -0600

    Update Emailapp.java

--- Emailapp.java
+++ Emailapp.java
@@ -2,8 +2,7 @@ package emailapp;
 
 public class Emailapp {
     public static void main(String[] args) {
-        Email em1 = new Email("John", "Smith");
-        em1.setAlternateEmail("js@gmail.com");
+        Email em1 = new Email();
         System.out.println(em1.showInfo());
     }
 }
